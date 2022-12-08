dd16859eeaee694bd50520398f6445e4c489a363
MirtheK <80322675+MirtheK@users.noreply.github.com>
2022-12-08 21:50:16 +0100

    new file

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import glob
-import os
-import numpy as np
-import cv2
-from scipy.ndimage import gaussian_filter
-from skimage import color, morphology
 import skimage
 from functions import *
 # from skimage.morphology import skeletonize
-from skimage import data
-import matplotlib.pyplot as plt
-from skimage.util import invert
 from tifffile import imwrite
-import numpy as np
 
 
 def main_func(path, string='*ch00*.tif'):
@@ -26,7 +16,7 @@ def main_func(path, string='*ch00*.tif'):
     dirlist
     S = 10
 
-    if (dirlist != 0):
+    if dirlist != 0:
         img = OCY_read_stack(string)
 
         # use only first N images
@@ -52,5 +42,7 @@ def main_func(path, string='*ch00*.tif'):
         imwrite(datapath + path + '/' + 'skeleton.tif', skeleton)
 
         graph = skel2Graph(skeleton, cells_wrong)
+    else:
+        pass
 
     return graph
